cf9d95d744de7fc5a01a7da64ca82f8e576e0126
koqps <101608143+koqps@users.noreply.github.com>
2026-09-20 04:23:36 -0700

    Add src/main/java/com/koqps/topka/mixin/OverlayTextureAccessor.java

---
+++ src/main/java/com/koqps/topka/mixin/OverlayTextureAccessor.java
@@ -0,0 +1,12 @@
+package com.koqps.topka.mixin;
+
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(OverlayTexture.class)
+public interface OverlayTextureAccessor {
+    @Accessor("texture")
+    DynamicTexture topka$getTexture();
+}
