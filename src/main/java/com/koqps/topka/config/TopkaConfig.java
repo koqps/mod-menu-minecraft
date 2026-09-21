cd98d3235e3a5064d662230abf7a3193f51f5b07
koqps <101608143+koqps@users.noreply.github.com>
2026-09-20 23:24:40 -0700

    Advance cosmetics migration to netherite armor renderer

--- src/main/java/com/koqps/topka/config/TopkaConfig.java
+++ src/main/java/com/koqps/topka/config/TopkaConfig.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public final class TopkaConfig {
-    public int cosmeticRendererVersion = 8;
+    public int cosmeticRendererVersion = 9;
     public Map<String, Boolean> modules = new HashMap<>();
     public List<WaypointConfig> waypoints = new ArrayList<>();
     public List<String> highlightedItems = new ArrayList<>(List.of(
