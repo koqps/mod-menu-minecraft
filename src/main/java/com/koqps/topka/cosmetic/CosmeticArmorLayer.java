a8127aba92c3c403bb198370ed878bd979871a5e
koqps <101608143+koqps@users.noreply.github.com>
2026-09-20 22:41:24 -0700

    Fix PlayerModel package for Minecraft 26.3

--- src/main/java/com/koqps/topka/cosmetic/CosmeticArmorLayer.java
+++ src/main/java/com/koqps/topka/cosmetic/CosmeticArmorLayer.java
@@ -5,7 +5,7 @@ import com.koqps.topka.asset.PackedMeshLibrary;
 import com.koqps.topka.asset.PackedMeshSubmitter;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.player.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
